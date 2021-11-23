ec1ffafc5a40b407a52f6dafcb08cfafb46876a5
Tonko <lordtonkamp@gmail.com>
2021-11-23 17:25:27 +0100

    Commit nove vjezbe

--- database.py
+++ database.py
@@ -3,10 +3,14 @@ con = sqlite3.connect('baza.db')
 
 cur = con.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS users
-               ( id INTEGER PRIMARY KEY  , name text, email text, password text, kontakt text,  created_at text)''')
+               ( id INTEGER PRIMARY KEY  , name text, email text, password text, kontakt text, login_count INTEGER DEFAULT 0, created_at text )''')
 
-cur.execute("INSERT INTO users (name, email, password, kontakt) VALUES (?,?,?,?)",('Dario',"ddemonjic8@gmail.com",'123456','0919332412'))
+cur.execute('''CREATE TABLE IF NOT EXISTS forgot_password
+                ( id INTEGER PRIMARY KEY, user_ime TEXT, hash TEXT, valid_until DATETIME, FOREIGN KEY (user_ime) REFERENCES users (name) )''')
+
+
 con.commit()
+
 
 
 con.close()
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import hashlib
 import sqlite3
+import time
 from datetime import date
 con = sqlite3.connect('baza.db')
 
@@ -7,14 +8,41 @@ con = sqlite3.connect('baza.db')
 def login():
         ime = input("Unesi ime: ")
         lozinka = input("Unesi lozinku: ")
-        
+        hash_object = hashlib.md5(lozinka.encode())
+        safelozinka = hash_object.hexdigest()
         cur = con.cursor()
         password=cur.execute("SELECT password FROM users WHERE name = ?", (ime,))
-        password=cur.fetchone();
-        print(password)
-        if(cur.rowcount>0):
-                print("Korisnik postoji \n")
+        password=cur.fetchone()
+        try:
+        
 
+                if(safelozinka==password[0]):
+                        login_count=cur.execute("SELECT login_count FROM users WHERE name = ?", (ime,))
+                        login_count=cur.fetchone()
+                        login_count=login_count[0] + 1
+                        cur.execute("UPDATE users SET login_count = ? WHERE name = ?", (login_count, ime))
+                        con.commit()               
+                        con.close()
+                        print("Uspješno ste logirani!")
+                else:
+                        print("Kriva lozinka.")
+                
+        except:
+                print("Korisnik ne postoji.")
+
+
+def mail():
+        try:
+                ime = input("Unesi ime: ")
+                email = input("Unesite email: ")
+                seconds = time.time()
+                cur = con.cursor()
+                cur.execute("INSERT INTO forgot_password (user_ime, hash) VALUES (?, ?)",(ime, seconds))
+                con.commit()
+                con.close()
+
+        except:
+                print("Korisnik ne postoji.")
         
 def registracija():
         ime = input("Unesi ime: ")
@@ -40,23 +68,22 @@ def registracija():
 
 def main():
     print("Dobrodošli u Unidu sustav!")
-    print("Za prijavu upišite broj 1, za registraciju broj 2:")
+    print("Za prijavu upišite broj 1, za registraciju broj 2, a ako ste zaboravili lozinku, broj 3:")
 
     unos = 0
 
-    while(unos!=1 and unos!=2):
+    while(unos!=1 and unos!=2 and unos!=3):
         unos = int(input("Unesite broj: "))
 
 
-    if(unos==2):
-        registracija()
+        if(unos==2):
+                registracija()                        
+                        
+        if(unos==1):
+                login()
 
-        
-        
-        
-    if(unos==1):
-        login()
-
+        if(unos==3):
+                mail()
 
 if __name__=="__main__":
     main()
